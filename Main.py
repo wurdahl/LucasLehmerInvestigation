156e48d75ce515a9c687b25890e6af5fa807c884
wurdahl <44688469+wurdahl@users.noreply.github.com>
2020-03-01 23:20:22 -0600

    Primality Test

---
+++ Main.py
@@ -0,0 +1,22 @@
+#This function will return true if the Merseinne number M(p) is prime using the Lucas-Lehmer method
+def LucLehIsPrime(p):
+    Mp = 2**(p)-1
+
+    s = 4
+
+    for i in range(p-2):
+        s = (s**2 - 2)%Mp
+
+    return(s==0)
+
+primeList = []
+
+outputFile = open("output.txt","w")
+
+if __name__ == "__main__":
+
+    for i in range(2000):
+        if LucLehIsPrime(i):
+            primeList.append(2**i - 1)
+
+    outputFile.write(str(primeList));
